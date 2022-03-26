53b89f9a4f4bc3c9b64aedd95781dac708e22191
Abdulazeez <abdulazeezoshinowo2@gmail.com>
2022-03-26 15:02:36 +0100

    added product details screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,5 +1,8 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:provider_shopping_app/providers/cart_provider.dart';
+import 'package:provider_shopping_app/screens/cart_screen.dart';
 
 import '../providers/products_provider.dart';
 
@@ -7,11 +10,6 @@ class ProductDetailScreen extends StatelessWidget {
   static const String id = 'product detail screen';
 
   const ProductDetailScreen({Key? key}) : super(key: key);
-
-  // final String title;
-  // final double price;
-
-  // ProductDetailScreen(this.title, this.price);
 
   @override
   Widget build(BuildContext context) {
@@ -21,9 +19,79 @@ class ProductDetailScreen extends StatelessWidget {
       context,
       listen: false,
     ).findById(productId);
+    final cart = context.read<Cart>();
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 15.0),
+            child: Badge(
+              badgeContent: Text(context.watch<Cart>().itemCount.toString()),
+              badgeColor: Colors.deepOrange,
+              child: IconButton(
+                  onPressed: () =>
+                      Navigator.of(context).pushNamed(CartScreen.id),
+                  icon: const Icon(Icons.shopping_cart)),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: 350,
+            child: Image.network(
+              loadedProduct.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            loadedProduct.title,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24.0,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 30.0,
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: Text(
+              loadedProduct.description,
+              textAlign: TextAlign.center,
+              softWrap: true,
+            ),
+          ),
+          const Spacer(),
+          Container(
+            margin: const EdgeInsets.symmetric(
+              horizontal: 10.0,
+            ),
+            padding: const EdgeInsets.only(
+              bottom: 30.0,
+            ),
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                cart.addItem(
+                    loadedProduct.id, loadedProduct.price, loadedProduct.title);
+              },
+              child: const Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Text(
+                  'ADD TO CART',
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
