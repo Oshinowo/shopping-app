c50308a2ce6310c878d7963055a0e12306b0e27b
Abdulazeez <abdulazeezoshinowo2@gmail.com>
2022-04-04 09:04:30 +0100

    make order item expandable

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,25 +1,63 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider_shopping_app/models/order.dart' as model;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   const OrderItem({Key? key, required this.orderItem}) : super(key: key);
 
   final model.OrderItem orderItem;
 
   @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  bool _isExpanded = false;
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.all(10.0),
-      child: ListTile(
-        title: Text('\$${orderItem.amount}'),
-        subtitle: Text(
-          DateFormat('dd-MM-yyyy hh:mm').format(orderItem.dateTime),
-        ),
-        trailing: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.expand_more),
-        ),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('\$${widget.orderItem.amount}'),
+            subtitle: Text(
+              DateFormat('dd-MM-yyyy hh:mm').format(widget.orderItem.dateTime),
+            ),
+            trailing: IconButton(
+              onPressed: () => setState(() {
+                _isExpanded = !_isExpanded;
+              }),
+              icon: _isExpanded
+                  ? const Icon(Icons.expand_less)
+                  : const Icon(Icons.expand_more),
+            ),
+          ),
+          if (_isExpanded)
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+                vertical: 5.0,
+              ),
+              height: min(widget.orderItem.products.length * 20 + 10, 100),
+              child: ListView(
+                children: widget.orderItem.products
+                    .map(
+                      (prod) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(prod.title),
+                          Text('${prod.quantity}x \$${prod.price}')
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+        ],
       ),
     );
   }
